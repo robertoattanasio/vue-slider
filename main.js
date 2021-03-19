7c02c5f3f5ddbbe3f5b2046a266bd0d18a77b69c
Roberto <r.attanasio90@gmail.com>
2021-03-19 15:45:13 +0100

    Esercizio base completato

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 var app = new Vue({
     el: '#root',
+    data: {
+        circle: ['far', 'fas'],
+        counter: 0,
+        images: ['img/img_1.jpg', 'img/img_2.jpg', 'img/img_3.jpg', 'img/img_4.jpg'],
+    },
+    methods: {
+        prevImg: function() {
+            this.counter--;
+            if (this.counter < 0) {
+                (this.counter = 3);
+            };
+        },
+        nextImg: function() {
+            this.counter++;
+            if (this.counter > 3) {
+                (this.counter = 0);
+            };
+        }
+    }
 });
