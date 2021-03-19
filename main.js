bac5531e0d3fb1fa99014bdb2a27c907bd28d1f1
Roberto <r.attanasio90@gmail.com>
2021-03-19 17:30:51 +0100

    Carousel automatico con reset del timer ad ogni click scorrimento foto

--- main.js
+++ main.js
@@ -1,23 +1,36 @@
 var app = new Vue({
     el: '#root',
     data: {
-        circleFull: 'fas fa-circle',
-        circleEmpty: 'far fa-circle',
+        circle: ['fas fa-circle', 'far fa-circle'],
         counter: 0,
         images: ['img/img_1.jpg', 'img/img_2.jpg', 'img/img_3.jpg', 'img/img_4.jpg'],
     },
     methods: {
         prevImg: function() {
             this.counter--;
+            clearInterval(this.interval);
+            this.startInterval();
+
             if (this.counter < 0) {
                 (this.counter = 3);
             };
         },
         nextImg: function() {
             this.counter++;
+            clearInterval(this.interval);
+            this.startInterval();
             if (this.counter > 3) {
                 (this.counter = 0);
             };
+        },
+
+        startInterval: function() {
+            this.interval = setInterval(() => {
+                this.nextImg();
+            }, 2000);
         }
+    },
+    created: function() {
+        this.startInterval();
     }
 });
